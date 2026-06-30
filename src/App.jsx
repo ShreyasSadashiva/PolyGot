0c1f7d5174aa666e7cab0307344f8e31d021243a
Shreyas_Achary <shreyasacharya8@gmail.com>
2026-06-30 01:24:36 +0100

    add basic translation

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,44 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
-import heroImg from "./assets/hero.png";
+import { useState, useEffect } from "react";
+import OpenAI from "openai";
+
 import "./App.css";
 
+const openai = new OpenAI({
+  apiKey: import.meta.env.VITE_AI_KEY,
+  baseURL: import.meta.env.VITE_AI_URL,
+  dangerouslyAllowBrowser: true,
+});
 function App() {
-  const [count, setCount] = useState(0);
+  useEffect(() => {}, []);
 
+  const [query, setQuerey] = useState("");
+
+  const onClickHandler = async () => {
+    console.log(query);
+    const response = await openai.chat.completions.create({
+      model: import.meta.env.VITE_AI_MODEL,
+      messages: [
+        {
+          role: "system",
+          content: `Translate the following text to French, Spanish, and German. Return the result in a JSON format with keys "French", "Spanish", and "German".`,
+        },
+        {
+          role: "user",
+          content: query,
+        },
+      ],
+    });
+    console.log(response.choices[0].message.content);
+  };
   return (
     <>
       <h1>polygot</h1>
+      <input
+        type="text"
+        placeholder="Enter text"
+        onChange={(e) => setQuerey(e.target.value)}
+      />
+      <button onClick={() => onClickHandler()}>Submit</button>
     </>
   );
 }
